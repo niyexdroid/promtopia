4b40cecdf9cf8720f8f2c7837cc96cf075ed9c67
niyexdroid <niyi.adegbola@outlook.com>
2024-08-11 07:30:51 +0100

    Added a suspense tag on update-prompt page to allow useSearchParams render in client side

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
@@ -50,13 +50,15 @@ const EditPrompt = () => {
     }
   }
   return (
-    <Form 
-      type='Edit'
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form 
+        type='Edit'
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   )
 }
 
